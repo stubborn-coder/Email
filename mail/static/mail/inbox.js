c716481d5947dae81e23ec82c84e1eeb587b1e40
Muhammad Bilal <mbbilal0612@gmail.com>
2023-07-05 16:27:30 +0500

    sending email works

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,9 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
+  document.querySelector('#compose-form').onsubmit = () => {send_email();}
+  // document.querySelector('#send-email').disabled = true;
   // By default, load the inbox
   load_mailbox('inbox');
+  
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log("emails:");
+    console.log(emails);
+
+    // ... do something else with emails ...
+  });
+
 });
 
 function compose_email() {
@@ -30,4 +42,40 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch('/emails/'+mailbox)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log("emails:");
+    console.log(emails);
+
+    // ... do something else with emails ...
+  });
+}
+
+function send_email() {
+  recipients = document.querySelector('#compose-recipients').value
+  subject = document.querySelector('#compose-subject').value
+  body = document.querySelector('#compose-body').value
+
+  print(recipients)
+  print(subject)
+
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
+  
+  
 }
